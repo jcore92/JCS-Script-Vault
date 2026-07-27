dbb0c6efdbd560ef61fd8fb129bfa9755488a538
jcore92 <138952572+jcore92@users.noreply.github.com>
2026-07-26 22:36:52 -0400

    fixes

---
+++ Resources/JS-Forge Support.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1. Start Flatpak in a completely isolated session
+setsid xdg-open https://t.me/jcorestudios/63 >/dev/null 2>&1 &
+
+# 2. Give the system a brief moment to map the process
+sleep 0.2
+
+# 3. Forcefully kill the parent script waiting on the "read" prompt
+kill -9 "$PPID"
+
+# 4. Exit this child script
+exit 0
