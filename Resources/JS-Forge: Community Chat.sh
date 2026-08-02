1b2832170d7ca69dc129d6565e72ac65aac24358
jcore92 <138952572+jcore92@users.noreply.github.com>
2026-08-02 19:57:43 -0400

    fixes

--- Resources/JS-Forge: Community Chat.sh
+++ Resources/JS-Forge: Community Chat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 jsf_no_pause="1"
 
-setsid xdg-open https://t.me/jcorestudios/63 >/dev/null 2>&1 &
+setsid xdg-open https://t.me/jcorestudios >/dev/null 2>&1 &
 disown 2>/dev/null || true
 
 #exit 0
